6f5fcbc90eddf724bcc5866bb1c7b9bc5f0918b0
PranavJeurkar <60751023+PranavJeurkar@users.noreply.github.com>
2020-09-18 21:18:52 +0530

    Add files via upload

---
+++ lib/love.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class Love extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Love Quotes'),
+        backgroundColor: Colors.blueGrey,
+      ),
+      body: ListView(
+        children: <Widget>[
+          Image.network('https://files.lifehacks.io/wp-content/uploads/Love-Quotes-for-Him-12.jpg'),
+          Image.network('https://cdn.graciousquotes.com/wp-content/uploads/2020/02/When-I-tell-you-I-love-you-I-am-not-saying-it-out-of-habit-I-am-reminding-you-that-you-are-my-life.jpg'),
+          Image.network('https://imagesvc.meredithcorp.io/v3/mm/image?q=85&c=sc&poi=%5B16200%2C16200%5D&w=1800&h=1800&url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F23%2F2020%2F02%2F04%2Fbest-love-quotes-romantic-inspirational-deep-leo-tolstoy.jpg'),
+          Image.network('https://wisdomquotes.com/wp-content/uploads/deep-love-quotes-the-love-we-give-away-is-the-only-love-we-keep-elbert-hubbard-1.jpg'),
+          Image.network('https://littlenivi.com/wp-content/uploads/2019/02/58-Short-Love-Quotes-About-Love-and-Life-Lessons-Inspire-1.jpg'),
+          Image.network('https://www.womangettingmarried.com/wp-content/uploads/2016/01/the-sweetest-love-quotes-013.jpg'),
+          Image.network('https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/valentines-day-love-quotes-12-1580936209.jpg'),
+          Image.network('https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSglkR59kGWBcbQeZQcwpypIHePWq2GG2f4xg&usqp=CAU'),
+          Image.network('https://image.shutterstock.com/image-photo/love-quotes-your-ones-600w-1009508560.jpg'),
+          Image.network('https://pixelsquote.net/wp-content/uploads/images1/Love-Quote-9.jpg'),
+          Image.network('https://i0.wp.com/www.techjunkie.com/wp-content/uploads/2019/04/2-True-Love-Quotes-for-Someone-Who-Waits.jpg?w=690&ssl=1'),
+          Image.network('https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTxglrAkPTl2v38CELs0M4U8d7QTf3V6X8z2w&usqp=CAU'),
+          Image.network('https://www.happybirthdaymsg.com/wp-content/uploads/2018/10/find-true-love-quotes-7.jpg'),
+          Image.network('https://lh3.googleusercontent.com/proxy/6CEIKdbfe_9rhguK01yoa60Z-sgf06VH3Fq4hj-kJXIpvZSIImeALCGD8ABYiJoFmQTvsFk9zT9I5aMRa0OwH72ljgz3891HNmeI-ljfUr-iXxCjibnF_bSsVwvptio'),
+          Image.network('https://i1.wp.com/bestlifeonline.com/wp-content/uploads/2017/11/Helen-Keller.jpg?resize=1024%2C1024&ssl=1'),
+          Image.network('https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F23%2F2020%2F02%2F04%2Fbest-love-quotes-romantic-inspirational-deep-victor-hugo.jpg'),
+          Image.network('https://1zl13gzmcsu3l9yq032yyf51-wpengine.netdna-ssl.com/wp-content/uploads/2018/06/02_Dr_Seuss_Quotes_Weird-1024x538.jpg'),
+          Image.network('https://cdn.lifehack.org/wp-content/uploads/2017/03/23061109/quotes-about-loving-yourself-bible.jpg'),
+          Image.network('https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSyVb43FHzI31sjHtUVKqBvIHZs1xR0Ml0r-g&usqp=CAU'),
+          Image.network('https://i.pinimg.com/originals/06/bd/24/06bd2436e5931efb369cab6e6e46b122.jpg'),
+          ],
+      ),
+    );
+  }
+}
