bb8be97d2768abe948f733bf6bfa0d2d81099950
PranavJeurkar <60751023+PranavJeurkar@users.noreply.github.com>
2020-09-18 19:43:54 +0530

    Add files via upload

---
+++ lib/friends.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class Friends extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Friendship Quotes'),
+        backgroundColor: Colors.blueGrey,
+      ),
+      body: ListView(
+        children: <Widget>[
+          Image.network('https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/l-m-mongomery-1561485794.png'),
+          Image.network('https://parade.com/wp-content/uploads/2019/11/Best-Friend-Quotes-01-680x430.jpg'),
+          Image.network('https://a2n9b7p7.stackpathcdn.com/wp-content/uploads/friendship_quotes_2.jpg'),
+          Image.network('https://www.keepinspiring.me/wp-content/uploads/2020/04/a-sweet-friendship-refreshes-the-soul-proverbs27-quote-min.jpg'),
+          Image.network('https://i.pinimg.com/originals/5e/63/46/5e6346ed2cb008456294abf7decc7848.jpg'),
+          Image.network('https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/best-friend-quotes-8-1557440291.jpg?crop=1xw:1xh;center,top&resize=480:*'),
+          Image.network('https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/friends-become-family-1561481784.png'),
+          Image.network('https://therightmessages.com/wp-content/uploads/2018/08/Friendship-Quotes-6.jpg'),
+          Image.network('https://r9t5u7b9.rocketcdn.me/wp-content/uploads/2017/04/friendship-quote-great-gift.png'),
+          Image.network('https://movemequotes.com/wp-content/uploads/2020/04/IMG_5129.jpg'),
+          Image.network('https://cdn-0.instagramcircus.com/wp-content/uploads/2020/01/funny-friend-captions.jpg'),
+          Image.network('https://im.idiva.com/content/2019/Aug/iDiva_Friendship_Quotes_1_5d443d760aea3.jpg'),
+          Image.network('https://www.yourtango.com/sites/default/files/styles/header_slider/public/display_list/mainfriends.png?itok=YVrO5k0M'),
+          Image.network('https://static.parade.com/wp-content/uploads/2019/11/Best-Friend-Quote-1.jpg'),
+          Image.network('https://1.bp.blogspot.com/-rYVLOAsRdZs/XwhEnYkBeuI/AAAAAAAAxGU/s1_QNDYAPZEHWAgni6KGqaGNijSKShLSwCLcBGAsYHQ/s1600/Short-friendship-quotes-that-will-make-your-bond-stronger%2B%25281%2529-min.jpg'),
+          Image.network('https://floweraura-blog-img.s3.ap-south-1.amazonaws.com/girlfriend+related+blog/friendship-quotes-that-express-the-true-meaning-of-friendship-cover.jpg'),
+          Image.network('https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/friendship-quotes-rekha-rani.jpg'),
+          Image.network('https://cdn-0.instagramcircus.com/wp-content/uploads/2020/01/true-friends-quotes.jpg'),
+          Image.network('https://images.fineartamerica.com/images/artworkimages/mediumlarge/3/a-friend-is-minions-cute-friendship-quotes-6-prar-kulasekara.jpg'),
+          Image.network('https://inspirationfeed.com/wp-content/uploads/2017/09/Friendship-Quotes-3-800x800.jpg'),
+          Image.network('https://i.pinimg.com/originals/9c/45/70/9c4570e5288d7342085ec3817b3a5dc9.png'),
+          Image.network('https://quotefancy.com/media/wallpaper/3840x2160/15504-Eleanor-Roosevelt-Quote-Many-people-will-walk-in-and-out-of-your.jpg'),
+          Image.network('https://www.keepinspiring.me/wp-content/uploads/2020/04/friendship-is-the-golden-thread-that-ties-the-heart-of-the-world-john-evelyn-quote-min-1024x1024.jpg'),
+          Image.network('https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTT5OJE1rEu0-XmhSb4GdBUEzbro9tqyYMcnA&usqp=CAU'),
+        ],
+      ),
+    );
+  }
+}
