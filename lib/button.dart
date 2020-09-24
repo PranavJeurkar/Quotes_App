0bd9252eba98ffc3fdab69320468003d4e74b1dd
PranavJeurkar <60751023+PranavJeurkar@users.noreply.github.com>
2020-09-24 23:59:59 +0530

    Update button.dart

--- lib/button.dart
+++ lib/button.dart
@@ -7,13 +7,21 @@ class Motivational extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-        title: const Text('Motivational Quotes'),
+          title: const Text('Motivational Quotes'),
     ),
       body: ListView(
         children: <Widget>[
-          Image.asset('assets/space.png'),
-          Image.asset('assets/friends.png'),
-          Image.asset('assets/space.png'),
+          Image.network('https://cdn5.vectorstock.com/i/1000x1000/05/89/inspirational-and-motivational-quotes-vector-4830589.jpg'),
+          Image.network('https://images-na.ssl-images-amazon.com/images/I/618mV3ssooL._SL1000_.jpg'),
+          Image.network('https://images-na.ssl-images-amazon.com/images/I/51HMGx0ITjL._SX258_BO1,204,203,200_.jpg'),
+          Image.network('https://cdn5.vectorstock.com/i/1000x1000/06/04/inspirational-and-motivational-quotes-vector-4830604.jpg'),
+          Image.network('https://static.wixstatic.com/media/65c081_cad83ab967c1490c8aba2d0b455c3b3e~mv2.jpg/v1/fill/w_632,h_632,al_c,q_85,usm_0.66_1.00_0.01/65c081_cad83ab967c1490c8aba2d0b455c3b3e~mv2.webp'),
+          Image.network('https://wp-en.oberlo.com/wp-content/uploads/2019/11/motivational-quotes-from-movies.jpg'),
+          Image.network('https://38.media.tumblr.com/601a1fa7b30120000dee2a2170e1154a/tumblr_n6ljgdOEbD1rprhpmo1_500.jpg'),
+          Image.network('https://img.theculturetrip.com/450x/wp-content/uploads/2017/01/inspirational-quotes-squre-3.png'),
+          Image.network('https://thumbs.dreamstime.com/z/motivational-quotes-life-abstract-background-inspirational-texts-blank-space-writing-your-own-text-wallpaper-font-164062676.jpg'),
+          Image.network('https://www.dailyfunnyquote.com/wp-content/uploads/2018/05/50-Motivational-Quotes-Life-Sayings-Inspire-You-to-Success-38.jpg'),
+          Image.network('https://motivationping.com/wp-content/uploads/2019/06/15-4.jpg') ,
         ],
       ),
     );
