1bb79370343c82ff782d00893757d9179294e296
PranavJeurkar <60751023+PranavJeurkar@users.noreply.github.com>
2020-09-28 23:24:23 +0530

    Update apj.dart

--- lib/apj.dart
+++ lib/apj.dart
@@ -1,2 +1,33 @@
+import 'package:flutter/material.dart';
 
+class APJ extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+      appBar: AppBar(
+        title: const Text('APJ Abdul Kalam Quotes'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Image.network('https://www.yourselfquotes.com/wp-content/uploads/APJ-Abdul-Kalam-Quotes14.png'),
+          Image.network('https://static.india.com/wp-content/uploads/2017/07/Untitled-collage-25.jpg?impolicy=Medium_Resize&w=1200&h=800'),
+          Image.network('https://www.theeducationmagazine.com/wp-content/uploads/2020/03/15.jpg'),
+          Image.network('https://www.scrolldroll.com/wp-content/uploads/2020/01/APJ-Abdul-Kalam-Quotes-10.jpg'),
+          Image.network('https://i.pinimg.com/originals/9e/28/21/9e2821e9cbd12438c4ca11b0b15a1de7.jpg'),
+          Image.network('https://www.shineinfinite.in/wp-content/uploads/2018/07/Dr.-A.P.J-Abdul-Kalam-quote.jpg'),
+          Image.network('https://i.pinimg.com/originals/f6/41/2d/f6412dd17f08e0507dbb271ee94d4642.jpg'),
+          Image.network('https://www.theeducationmagazine.com/wp-content/uploads/2020/03/7-5.jpg'),
+          Image.network('https://images-na.ssl-images-amazon.com/images/I/51PkI16WEZL.jpg'),
+          Image.network('https://rukminim1.flixcart.com/image/352/352/jc0ttow0-1/poster/5/g/z/medium-pw-apj-abdul-kalam-quote-wall-poster-13-19-inches-matte-original-imafyng3f29fazcc.jpeg?q=70'),
+          Image.network('https://i.pinimg.com/originals/d5/70/cc/d570cc3ae55cedd1cdc8f832ce07c056.jpg'),
+          Image.network('https://www.azquotes.com/vangogh-image-quotes/51/43/Quotation-Abdul-Kalam-All-Birds-find-shelter-during-a-rain-But-Eagle-avoids-51-43-89.jpg'),
+          Image.network('https://i.pinimg.com/originals/5a/d4/9a/5ad49ab7274317dcd0d5fed612c06e66.jpg'),
+          Image.network('https://images.news18.com/ibnlive/uploads/2018/10/APJ-Abdul-Kalams-birth-Anniversary-3.jpg?impolicy=website&width=400&height=0'),
+          Image.network('https://dheivegam.com/wp-content/uploads/2018/07/adbul_kalam_quotes_3.jpg'),
+          Image.network('https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQwjpBusYKukoW128TcDxpmK44rOo4PN09dGw&usqp=CAU'),
+        ],
+      ),
+
+    );
+  }
+}
 
