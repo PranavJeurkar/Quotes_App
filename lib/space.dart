e6895b34498403828863b5db877e1454febff391
PranavJeurkar <60751023+PranavJeurkar@users.noreply.github.com>
2020-10-06 19:34:09 +0530

    Update space.dart

--- lib/space.dart
+++ lib/space.dart
@@ -5,7 +5,7 @@ class Space extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Space'),
+        title: const Text('Space Quotes'),
       ),
       body: ListView(
         children: <Widget>[
