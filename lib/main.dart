acf0741cf17a8273893c201e4da1736248b88d30
PranavJeurkar <60751023+PranavJeurkar@users.noreply.github.com>
2020-10-22 17:59:28 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Quotes',
+      title: 'Quotes App',
       home: MyHomePage(),
     );
   }
