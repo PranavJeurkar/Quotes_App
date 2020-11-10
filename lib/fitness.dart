cc3827c1b987df5790c5427feaf972f0587f9609
PranavJeurkar <60751023+PranavJeurkar@users.noreply.github.com>
2020-11-10 20:14:36 +0530

    Update fitness.dart

--- lib/fitness.dart
+++ lib/fitness.dart
@@ -5,7 +5,7 @@ class Fitness extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Fitness Quotes'),
+        title: const Text('Fitness Quotes '),
       ),
       body: ListView(
         children: <Widget>[
