5f3e2246a57a54df120db5304414b9e0aca357f3
PranavJeurkar <60751023+PranavJeurkar@users.noreply.github.com>
2020-12-03 21:48:28 +0530

    Update gn.dart

--- lib/gn.dart
+++ lib/gn.dart
@@ -13,7 +13,7 @@ class Adventure extends StatelessWidget {
           Image.network(''),
           Image.network(''),
           Image.network(''),
-          Image.network('https://images.squarespace-cdn.com/content/v1/5beb0a44f2e6b1113f9519d9/1579603488689-2EUSFEXPTVO2VTJ2H9MM/ke17ZwdGBToddI8pDm48kPoswlzjSVMM-SxOp7CV59BZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZamWLI2zvYWH8K3-s_4yszcp2ryTI0HqTOaaUohrI8PIeQMKeWYgwh6Mn73n2eZmZLHHpcPIxgL2SArp_rN2M_AKMshLAGzx4R3EDFOm1kBS/Couple+travel+quotes+adventure.jpg'),
+          Image.network(''),
           Image.network('https://img.picturequotes.com/2/5/4118/the-biggest-adventure-you-can-take-is-to-live-the-life-of-your-dreams-quote-1.jpg'),
           Image.network('https://i.pinimg.com/736x/2d/75/a8/2d75a81fd31860976778621b75b6023f.jpg'),
           Image.network('https://askforadventure.com/wp-content/uploads/2018/08/inspirational-quote-1-683x1024.jpg'),
