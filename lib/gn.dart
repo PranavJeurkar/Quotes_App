26e720037ddf055fb8c7deff5c3409e68aaa68f3
PranavJeurkar <60751023+PranavJeurkar@users.noreply.github.com>
2020-12-04 22:43:50 +0530

    Update gn.dart

--- lib/gn.dart
+++ lib/gn.dart
@@ -16,7 +16,7 @@ class Adventure extends StatelessWidget {
           Image.network(''),
           Image.network(''),
           Image.network(''),
-          Image.network('https://askforadventure.com/wp-content/uploads/2018/08/inspirational-quote-1-683x1024.jpg'),
+          Image.network(''),
           Image.network('https://bayart.org/wp-content/uploads/2018/11/adventure-quotes.jpg'),
           Image.network('https://travelwiththesmile.com/wp-content/uploads/2018/04/adventure-quotes-Travel-with-the-Smile-60-683x1024.jpg'),
           Image.network('https://images.squarespace-cdn.com/content/v1/5beb0a44f2e6b1113f9519d9/1579603447596-PX52O9OL4JIMYT9YVCW9/ke17ZwdGBToddI8pDm48kPoswlzjSVMM-SxOp7CV59BZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZamWLI2zvYWH8K3-s_4yszcp2ryTI0HqTOaaUohrI8PIeQMKeWYgwh6Mn73n2eZmZLHHpcPIxgL2SArp_rN2M_AKMshLAGzx4R3EDFOm1kBS/Adventure+quotes.jpg'),
